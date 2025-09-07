1cd4a01eed9febe9816298bd1fe5cf53f9d6b73f
Michael Peterson <peterson@discordapp.com>
2025-09-06 19:52:02 -0700

    Getting closer

--- module.nix
+++ module.nix
@@ -13,34 +13,75 @@ in
 
     host = lib.mkOption {
       type = lib.types.str;
-      default = "localhost";
-      description = "What host to bind to";
+      default = "0.0.0.0";
+      description = "The host pelican panel will listen on.";
     };
 
     port = lib.mkOption {
       type = lib.types.int;
-      default = "8080";
-      description = "What port to bind to";
+      default = 8080;
+      description = "The port the web server will listen on.";
+    };
+
+    phpFpmPort = lib.mkOption {
+      type = lib.types.int;
+      default = 9000;
+      description = "The port the PHP FMP will listen on.";
+    };
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "pelican-panel";
+      description = "The user pelican panel should run as.";
+    };
+
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = "pelican-panel";
+      description = "The group pelican panel should run as.";
+    };
+
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to open the pelican panel port in the firewall.";
     };
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.services.pelican-panel = {
-      enable = cfg.enable;
-      description = "Pelican Panel";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${pelicanPanelPkg}/bin/pelican-panel";
-        Environment = [
-          "HOST=${cfg.host}"
-          "PORT=${toString cfg.port}"
-        ];
+    services.phpfpm.pools.pelican-panel = {
+      user = cfg.user;
+      group = cfg.group;
+      listen = "${cfg.host}:${toString cfg.phpFpmPort}";
+      phpPackage = pelicanPanelPkg.php;
+
+      settings = {
+        "pm" = "dynamic";
+        "pm.max_children" = 32;
+        "pm.max_requests" = 500;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 2;
+        "pm.max_spare_servers" = 5;
       };
     };
 
-    services.phpfpm.pools.pelican-panel = {
-      user = "pelican-panel";
-      phpPackage = pelicanPanelPkg.php;
+    services.nginx = {
+      enable = true;
+
+      virtualHosts."pelican-panel" = {
+        listen = [
+          {
+            addr = cfg.host;
+            port = cfg.port;
+          }
+        ];
+        root = "${pelicanPanelPkg}/share/php/pelican-panel/public";
+        locations."/".extraConfig = ''
+          try_files $uri /index.php?$query_string;
+        '';
+      };
     };
+
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.port ];
   };
 }
